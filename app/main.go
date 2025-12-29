6ad13d5a89e0b19412dc44a15ce855955e41b9c0
David Olowomeye <olowomeyedavid242@gmail.com>
2025-12-29 07:11:38 +0000

    Responded to multiple PING commands

--- app/main.go
+++ app/main.go
@@ -4,23 +4,49 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"io"
+
 )
 
 var _ = net.Listen
 var _ = os.Exit
 
-func main() {
-	fmt.Println("Logs from your program will appear here!")
-	
+func main() {	
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
+
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+
 	conn, err := l.Accept()
+
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	conn.Write([]byte("+PONG\r\n"))
+
+	multipleConn(conn)
+}
+
+
+func multipleConn(conn net.Conn) {
+	defer conn.Close()
+
+	buffer := make([]byte, 1024)
+
+	for {		
+		_, err := conn.Read(buffer)
+
+		if err != nil {
+			if err == io.EOF{
+				fmt.Println("End of file: No more connection")			
+			} else {
+				fmt.Println("Error: ", err)			
+			}
+			return
+		}
+
+		conn.Write([]byte("+PONG\r\n"))
+	}	
 }
