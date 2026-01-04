01a88a25e72bc406b9002f7dc1dd0ed5722d054c
David Olowomeye <olowomeyedavid242@gmail.com>
2026-01-04 07:15:22 +0000

    Added a support for the SET and GET commands

--- app/main.go
+++ app/main.go
@@ -6,10 +6,16 @@ import (
 	"net"
 	"os"
 	"strings"
+	"sync"
 )
 
 var _ = net.Listen
 var _ = os.Exit
+var (
+	storage = make(map[string]string)
+	mu 	sync.RWMutex
+)
+
 
 func main() {	
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
@@ -32,6 +38,7 @@ func main() {
 }
 
 
+
 func multipleConn(conn net.Conn) {
 	defer conn.Close()
 	
@@ -39,7 +46,7 @@ func multipleConn(conn net.Conn) {
 		buffer := make([]byte, 1024)
 
 		n, err := conn.Read(buffer)
-		fmt.Println(n)
+		// fmt.Println(n)
 
 		if err != nil {
 			if err == io.EOF{
@@ -50,20 +57,48 @@ func multipleConn(conn net.Conn) {
 			return
 		}
 		
+		// GET Input: *3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n
+		// SET Input: *2\r\n$3\r\nGET\r\n$3\r\nfoo\r\n
 		input := string(buffer[:n])
-		// fmt.Println(input)
+		parts := strings.Split(input, "\r\n")
+		command := strings.ToUpper(parts[2])
 
-		if strings.Contains(strings.ToUpper(input), "ECHO") {
-			parts := strings.Split(input, "\r\n")
-
+		switch command {
+		case "PING":
+			conn.Write([]byte("+PONG\r\n"))			
+		case "ECHO":
 			message := parts[4]
-
 			response := fmt.Sprintf("$%d\r\n%s\r\n", len(message), message)			
-			
 			conn.Write([]byte(response))
-		} else {
-			conn.Write([]byte("+PONG\r\n"))
+		case "SET":
+			key := parts[4]
+			value := parts[6]
+
+			mu.Lock()
+			storage[key] = value
+			mu.Unlock()
+
+			conn.Write([]byte("+OK\r\n"))
+		case "GET":
+			key := parts[4]
+
+			mu.RLock()
+			value, exists := storage[key]
+			mu.RUnlock()
+
+			if exists {
+				// Send Bulk String: "$length\r\nvalue\r\n"
+				response := fmt.Sprintf("$%d\r\n%s\r\n", len(value), value)			
+				conn.Write([]byte(response))
+			} else {
+				// Redis Null Bull String 
+				conn.Write([]byte("$-1\r\n"))
+			}
+			
+		default:
+			fmt.Println("Unknown command:", command)
 		}
+		
 
 	}	
 }
